4b72d9f0eb375df1aed1b34fcbee3a087165d1d0
ShubhzDev <shubhaangiunity@gmail.com>
2024-12-19 04:56:44 +0530

    Update solana.ts

--- src/config/solana.ts
+++ src/config/solana.ts
@@ -5,6 +5,6 @@ export const SOLANA_RPC_ENDPOINT = clusterApiUrl(SOLANA_NETWORK);
 
 // Replace with your deployed program ID
 export const STAKING_PROGRAM_ID = new PublicKey('HD2Ya6ng1icMrKyxdB74xC5fbud2zYyFwieQcbtGQS3k');
-export const INTW_MINT = new PublicKey('74YvwAyBfCj73xm6mKxwxrdY87C1wzHY564KjVsWyhDg');
+export const INTW_MINT = new PublicKey('AxUiAtzQMvWP2zCXVyd7YGTRnjE4KitQy6PXu33YrMcx');
 
 export const APY = 5; // 5% APY
